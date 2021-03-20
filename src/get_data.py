70a9b8c13f3d3d20e23f590689d4229277eb1018
aby philip <abyphilipaby1996@gmail.com>
2021-03-20 13:49:08 +0530

    new changes

--- src/get_data.py
+++ src/get_data.py
@@ -17,7 +17,7 @@ def get_data(config_path):
     data_path = config["data_source"]["s3_source"]
     #print(data_path)
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
-    #print(df.head())
+    #nt(df.head())
     return df
 
 
